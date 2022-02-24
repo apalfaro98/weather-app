8f33d347870ce377e119f611e56160f4912e4933
Angelica Perez <apalfaro98@gmail.com>
2022-02-24 13:37:55 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,4 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <p class="mt-5 font-bold">Hola hola hola</p>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        colors: {
+            dark: {
+                darker: '#100E1D',
+                default: '#1E213A',
+                lighter: '#585676',
+            },
+            accent: '#3C47E9',
+        },
+        extend: {},
+    },
+    plugins: [],
+};
